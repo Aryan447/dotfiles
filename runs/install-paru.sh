06910eef55958ccd8ff9afb9bdee4f259b844192
Aryan447 <singharyan4477@gmail.com>
2025-07-12 18:04:50 +0530

    automated dev commit

--- runs/install-paru.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo pacman -Syu --needed base-devel git -y
-
-git clone https://aur.archlinux.org/paru.git
-
-cd paru
-
-makepkg -si
--- runs/setup-hyprland.sh
+++ runs/setup-hyprland.sh
@@ -3,3 +3,5 @@
 sudo pacman -Syu hyprland hyprpaper hyprlock hyprshot rofi-wayland nautilus eog hyprpolkitagen swww --noconfirm --needed
 
 paru -Syu hyprnotify wlogout wshowkeys-mao-git overskride --noconfirm --needed
+
+sudo pacman -S xdg-desktop-portal-hyprland
