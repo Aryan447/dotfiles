e0c3ee400ec144c3bfa759b04a6b091fd0f579a1
Aryan447 <singharyan4477@gmail.com>
2025-08-13 12:03:03 +0530

    automated dev commit

--- runs/setup-hyprland.sh
+++ runs/setup-hyprland.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -Syu --noconfirm hyprland hyprpaper hyprlock swaync hyprshot hypridle udiskie rofi-wayland nautilus eog hyprpolkitagent blueman batsignal swww xdg-desktop-portal-hyprland --needed
+sudo pacman -Syu --noconfirm hyprland waybar hyprpaper btop impala hyprlock swaync hyprshot hypridle udiskie rofi-wayland nautilus eog hyprpolkitagent blueberry batsignal swww xdg-desktop-portal-hyprland --needed
 
 if command -v paru &> /dev/null; then
     paru -Syu --noconfirm hyprnotify wlogout wshowkeys-mao-git waypaper cliphist networkmanager-dmenu dmenu-bluetooth --needed
