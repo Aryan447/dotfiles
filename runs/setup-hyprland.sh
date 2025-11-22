4c971f41cd1a262503325649d2ea8bbeed49d28f
Aryan447 <singharyan4477@gmail.com>
2025-11-22 10:40:35 +0530

    automated dev commit

--- runs/setup-hyprland.sh
+++ runs/setup-hyprland.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syu --noconfirm hyprland waybar hyprpaper btop impala hyprlock swaync hyprshot hypridle udiskie rofi-wayland nautilus eog hyprpolkitagent blueberry batsignal swww xdg-desktop-portal-hyprland --needed
 
 if command -v paru &> /dev/null; then
-    paru -Syu --noconfirm hyprnotify wlogout wshowkeys-mao-git waypaper cliphist networkmanager-dmenu dmenu-bluetooth --needed
+    paru -Syu --noconfirm hyprnotify wlogout wshowkeys-mao-git bluetui waypaper cliphist --needed
 fi
 
 sudo systemctl enable NetworkManager
